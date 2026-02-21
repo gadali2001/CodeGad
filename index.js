192894a3bd38ef1e98a2fa0ee95f91b9c046bea9
gadali2001 <enggadali@gmail.com>
2026-02-21 13:38:19 +0200

    security edit

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ const app = express();
 
 app.use(cors(
     {
-        origin: "https://codegad.vercel.app",
+        origin: ["https://codegad.vercel.app", "http://localhost:3000"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"],
     }
 ));
 app.use(express.json());
